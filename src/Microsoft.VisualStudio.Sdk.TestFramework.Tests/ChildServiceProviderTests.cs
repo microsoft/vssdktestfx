01daf5c41688f281fd6b0f2767906cd761a6e1de
Andrew Arnott <andarno@microsoft.com>
2015-07-26 10:21:51 -0700

    Add ChildServiceProvider

---
+++ src/Microsoft.VisualStudio.Sdk.TestFramework.Tests/ChildServiceProviderTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Sdk.TestFramework;
+using Microsoft.VisualStudio.Shell.Interop;
+using Moq;
+using Xunit;
+
+public class ChildServiceProviderTests
+{
+    private readonly IServiceProvider parentServiceProvider;
+
+    public ChildServiceProviderTests()
+    {
+        var parentSP = new Mock<IServiceProvider>();
+        parentSP.Setup(sp => sp.GetService(typeof(SVsSolution)))
+            .Returns(new Mock<IVsSolution>().Object);
+        this.parentServiceProvider = parentSP.Object;
+    }
+
+    [Fact]
+    public void OffersOwnServices()
+    {
+        var childServiceProvider = new ChildServiceProvider(this.parentServiceProvider);
+        childServiceProvider.AddService(typeof(IVsProject), new Mock<IVsProject>().Object);
+        Assert.NotNull(childServiceProvider.GetService(typeof(IVsProject)));
+    }
+
+    [Fact]
+    public void OffersParentServices()
+    {
+        var childServiceProvider = new ChildServiceProvider(this.parentServiceProvider);
+        Assert.IsAssignableFrom<IVsSolution>(childServiceProvider.GetService(typeof(SVsSolution)));
+    }
+}
---
+++ src/Microsoft.VisualStudio.Sdk.TestFramework/ChildServiceProvider.cs
@@ -0,0 +1,76 @@
+﻿/********************************************************
+*                                                        *
+*   © Copyright (C) Microsoft. All rights reserved.      *
+*                                                        *
+*********************************************************/
+
+namespace Microsoft.VisualStudio.Sdk.TestFramework
+{
+    using System;
+    using System.Collections.Immutable;
+    using Shell;
+
+    /// <summary>
+    /// An <see cref="IServiceProvider"/> implementation that inherits services
+    /// from another.
+    /// </summary>
+    public class ChildServiceProvider : IServiceProvider
+    {
+        /// <summary>
+        /// The parent service provider to call when we're requested
+        /// for services we don't have locally.
+        /// </summary>
+        private readonly IServiceProvider parent;
+
+        /// <summary>
+        /// The local services that have been added to this instance.
+        /// </summary>
+        private ImmutableDictionary<Type, object> services = ImmutableDictionary<Type, object>.Empty;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChildServiceProvider"/> class.
+        /// </summary>
+        public ChildServiceProvider()
+            : this(ServiceProvider.GlobalProvider)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChildServiceProvider"/> class.
+        /// </summary>
+        /// <param name="globalServiceProvider">The global service provider.</param>
+        public ChildServiceProvider(IServiceProvider globalServiceProvider)
+        {
+            Requires.NotNull(globalServiceProvider, nameof(globalServiceProvider));
+
+            this.parent = globalServiceProvider;
+        }
+
+        /// <summary>
+        /// Adds a service to the service provider.
+        /// </summary>
+        /// <param name="serviceType">The service type identity.</param>
+        /// <param name="service">The service instance.</param>
+        public void AddService(Type serviceType, object service)
+        {
+            if (!ImmutableInterlocked.TryAdd(ref this.services, serviceType, service))
+            {
+                throw new InvalidOperationException("Service already added.");
+            }
+        }
+
+        /// <inheritdoc />
+        public object GetService(Type serviceType)
+        {
+            Requires.NotNull(serviceType, nameof(serviceType));
+
+            object service;
+            if (!this.services.TryGetValue(serviceType, out service))
+            {
+                service = this.parent.GetService(serviceType);
+            }
+
+            return service;
+        }
+    }
+}
