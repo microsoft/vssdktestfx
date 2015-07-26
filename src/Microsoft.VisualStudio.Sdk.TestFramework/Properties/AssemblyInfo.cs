2234f45c26cbd950496d708da2cf581a71e5986a
Andrew Arnott <andarno@microsoft.com>
2015-07-26 10:58:51 -0700

    Add Nerdbank.GitVersioning package

--- src/Microsoft.VisualStudio.Sdk.TestFramework/Properties/AssemblyInfo.cs
+++ src/Microsoft.VisualStudio.Sdk.TestFramework/Properties/AssemblyInfo.cs
@@ -11,24 +11,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a62e3d92-0b5c-4d53-9269-eac253bbcdee")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
