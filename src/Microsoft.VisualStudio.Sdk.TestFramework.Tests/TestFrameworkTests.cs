594a5402b0cde74328e62742ee50d548746a6c76
Andrew Arnott <andarno@microsoft.com>
2015-07-26 10:21:12 -0700

    Remove namespace from xunit tests

--- src/Microsoft.VisualStudio.Sdk.TestFramework.Tests/TestFrameworkTests.cs
+++ src/Microsoft.VisualStudio.Sdk.TestFramework.Tests/TestFrameworkTests.cs
@@ -4,56 +4,55 @@
 *                                                        *
 *********************************************************/
 
-namespace Microsoft.VisualStudio.Sdk.TestFramework.Tests
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Sdk.TestFramework;
+using Microsoft.VisualStudio.Sdk.TestFramework.Tests;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Threading;
+using Xunit;
+using Task = System.Threading.Tasks.Task;
+
+[Collection(MockedVS.Collection)]
+public class TestFrameworkTests
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Runtime.InteropServices;
-    using System.Text;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Shell;
-    using Threading;
-    using Xunit;
-    using Task = System.Threading.Tasks.Task;
+    private readonly MefHosting mef;
 
-    [Collection(MockedVS.Collection)]
-    public class TestFrameworkTests
+    public TestFrameworkTests(GlobalServiceProvider sp, MefHosting mef)
     {
-        private readonly MefHosting mef;
+        this.mef = mef;
+        sp.Reset();
+    }
 
-        public TestFrameworkTests(GlobalServiceProvider sp, MefHosting mef)
+    [Fact]
+    public async Task MainThreadHandling()
+    {
+        if (ThreadHelper.JoinableTaskContext.MainThread == Thread.CurrentThread)
         {
-            this.mef = mef;
-            sp.Reset();
+            // Get off the "main thread" so we can switch back.
+            await TaskScheduler.Default;
+            Assert.NotSame(ThreadHelper.JoinableTaskContext.MainThread, Thread.CurrentThread);
         }
 
-        [Fact]
-        public async Task MainThreadHandling()
-        {
-            if (ThreadHelper.JoinableTaskContext.MainThread == Thread.CurrentThread)
-            {
-                // Get off the "main thread" so we can switch back.
-                await TaskScheduler.Default;
-                Assert.NotSame(ThreadHelper.JoinableTaskContext.MainThread, Thread.CurrentThread);
-            }
+        ThreadHelper.ThrowIfOnUIThread();
+        Assert.Throws<COMException>(() => ThreadHelper.ThrowIfNotOnUIThread());
 
-            ThreadHelper.ThrowIfOnUIThread();
-            Assert.Throws<COMException>(() => ThreadHelper.ThrowIfNotOnUIThread());
+        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+        Assert.Same(ThreadHelper.JoinableTaskContext.MainThread, Thread.CurrentThread);
 
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-            Assert.Same(ThreadHelper.JoinableTaskContext.MainThread, Thread.CurrentThread);
+        ThreadHelper.ThrowIfNotOnUIThread();
+        Assert.Throws<COMException>(() => ThreadHelper.ThrowIfOnUIThread());
+    }
 
-            ThreadHelper.ThrowIfNotOnUIThread();
-            Assert.Throws<COMException>(() => ThreadHelper.ThrowIfOnUIThread());
-        }
-
-        [Fact]
-        public async Task TestAssemblyIsInMefCatalog()
-        {
-            var ep = await this.mef.CreateExportProviderAsync();
-            Assert.NotNull(ep.GetExportedValue<SomeMefExport>());
-        }
+    [Fact]
+    public async Task TestAssemblyIsInMefCatalog()
+    {
+        var ep = await this.mef.CreateExportProviderAsync();
+        Assert.NotNull(ep.GetExportedValue<SomeMefExport>());
     }
 }
