963c7be883bfe07a4bbd80521dd10075d5b4d78a
Andrew Arnott <andrew.arnott@microsoft.com>
2024-04-05 12:30:32 -0600

    Bump .NET targeting to .NET 8

---
+++ src/Microsoft.VisualStudio.Sdk.TestFramework/Usings.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+global using System.Collections.Immutable;
+global using System.Runtime.InteropServices;
+global using Microsoft.VisualStudio.Shell;
+#if NETFRAMEWORK || WINDOWS
+global using Microsoft.VisualStudio.Shell.Interop;
+#endif
+global using Microsoft.VisualStudio.Threading;
